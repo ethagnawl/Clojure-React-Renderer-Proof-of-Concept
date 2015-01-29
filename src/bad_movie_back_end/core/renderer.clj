467b9d581ca67070ac67858a21390d0bc8518760
Peter Doherty <pdoherty926@gmail.com>
2015-01-29 15:53:03 -0500

    moving react render code into its own namespace

---
+++ src/bad_movie_back_end/core/renderer.clj
@@ -0,0 +1,37 @@
+(ns bad-movie-back-end.core.renderer
+
+  (:import [
+            javax.script
+            ScriptEngineManager])
+
+  (:require
+    [clojure.data.json :as json]
+    [bad-movie-back-end.core.views.bad-movie-back-end-layout :refer [common-layout]]))
+
+(defn- local-script [path]
+  (clojure.java.io/resource (str "public/javascripts/" path)))
+
+(defn- create-engine
+  "Creates a new nashorn script engine and loads dependencies into its context."
+  [dependencies]
+  (let [nashorn (.getEngineByName (ScriptEngineManager.) "nashorn")
+        scripts (map #(str "load('" % "');") dependencies)]
+    (.eval nashorn "var global = this;")
+    (doseq [script scripts] (.eval nashorn script))
+    nashorn))
+
+(def ^:private react "http://cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react.min.js")
+
+(def ^:private app (local-script "app.js"))
+
+(def ^:private nashorn (create-engine [react app]))
+
+(def ^:private dummy-data {:name "Pete"})
+
+(defn- react-render [component]
+  (.eval nashorn (str "React.renderComponentToString(" component ")")))
+
+(defn- dummy-component [data]
+  (str "MyApp.create(" (json/write-str data) ")"))
+
+(defn render [_] (react-render (dummy-component dummy-data)))
--- src/bad_movie_back_end/core/routes/bad_movie_back_end_routes.clj
+++ src/bad_movie_back_end/core/routes/bad_movie_back_end_routes.clj
@@ -8,36 +8,9 @@
     [clojure.data.json :as json]
     [ring.util.response :as response]
     [compojure.core :refer :all]
-    [bad-movie-back-end.core.views.bad-movie-back-end-layout :refer [common-layout]]))
+    [bad-movie-back-end.core.renderer :refer [render]]))
 
-(defn create-engine
-  "Creates a new nashorn script engine and loads dependencies into its context."
-  [dependencies]
-  (let [nashorn (.getEngineByName (ScriptEngineManager.) "nashorn")
-        scripts (map #(str "load('" % "');") dependencies)]
-    (.eval nashorn "var global = this;")
-    (doseq [script scripts] (.eval nashorn script))
-    nashorn))
-
-(def react "http://cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react.min.js")
-
-(defn local-script [path]
-  (clojure.java.io/resource (str "public/javascripts/" path)))
-
-(def app (local-script "app.js"))
-
-(def nashorn (create-engine [react app]))
-
-(def dummy-data {:name "Pete"})
-
-(defn dummy-component [data]
-  (str "MyApp.create(" (json/write-str data) ")"))
-
-(defn react-render [component]
-  (.eval nashorn (str "React.renderComponentToString(" component ")")))
-
-(defn get-route [request]
-  (common-layout (react-render (dummy-component dummy-data))))
+(defn get-route [request] (render request))
 
 (defroutes bad-movie-back-end-routes
            (GET  "/" [] get-route))
